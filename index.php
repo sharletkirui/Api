e3628b274d52c63165a607ea98522ac44bfcdbda
sharletkirui <sharlet.kirui@strathmore.edu>
2024-09-02 08:48:46 +0300

    Edited index.php and load.php and deleted fnc.php

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=h2, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2>This is my First Page</h2>
-    <?php 
-    require_once "load.php";
-    print $Obj->user_age("Alex",2005);
-    print '<br>';
-    ?>
-</body>
-</html>
+<?php
+require_once "load.php";
+$ObjLayouts->heading();
+$ObjMenus->main_menu();
+print "<br><br><br><br><br>";
+$ObjLayouts->footer();
--- load.php
+++ load.php
@@ -1,3 +1,34 @@
 <?php 
-require_once "fnc.php";
-$Obj = new fnc();
+//Class Auto Load 
+function ClassAutoLoad($ClassName){
+    $directories = ["forms","processes","structure","tables","global","store"];
+    foreach($directories AS $dir){
+        $FileName = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir .  DIRECTORY_SEPARATOR . $ClassName . '.php';
+        
+        if(file_exists($FileName) AND is_readable($FileName)){
+         require $FileName;
+        }
+   }
+    
+}
+spl_autoload_register('ClassAutoLoad');
+
+//require_once "structure/layouts.php";
+$ObjLayouts = new layouts();
+
+//require_once "structure/menus.php";
+$ObjMenus = new menus();
+//$Obj = new fnc();
+//print "me";
+// $path = dirname(__FILE__) . DIRECTORY_SEPARATOR.  ;
+//print $path;
+//$arr = ["Green","Black","Red","White"];
+//foreach($arr AS $color){
+    //print $color . "<br>";
+//}
+//$file = "index.php";
+//if(is_readable($file)){
+//    print "Yes";
+//}else{
+//    print "No";
+//}
