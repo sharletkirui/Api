ecc9813bd2e32c7398927abf6560be720669d733
sharletkirui <sharlet.kirui@strathmore.edu>
2024-09-08 18:43:33 +0300

    Added right side menu

--- structure/menus.php
+++ structure/menus.php
@@ -1,19 +1,24 @@
-<?php 
-class menus{
-    public function main_menu(){
+<?php
+    class menus{
+        public function main_menu(){
 ?>
-        <div class="topnav">
-        <?php 
-        $obj = new Fnc();
-        print '<a href="./">Welcome ' . $obj->computer_user("Sharlet") . '</a>';
-        ?>    
-        <a href="./">Home</a>
-        <a href="./">About </a>
-        <a href="./">Our Projects</a>
-        <a href="./">Our Portforlio</a>
-        <a href="./">Blog</a>
-        <a href="./">Contact Us</a>
-        </div>
-<?php        
+            <div class="topnav">
+                <a href="./">Home</a>
+                <a href="aboutus.php">About Us</a>
+                <a href="">Our Projects</a>
+                <a href="">Our Portofio</a>
+                <a href="">Blog</a>
+                <a href="">Contact Us</a>
+                <?php $this->main_right_side_menu(); ?>
+            </div>
+<?php
+    }
+        public function main_right_side_menu(){
+?>
+            <div class="topnav-right">
+                <a href="">Sign Up</a>
+                <a href="">Sign In</a>
+            </div>
+<?php
     }
 }
